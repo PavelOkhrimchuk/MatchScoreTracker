d971acd81b4ff13713899a91a1a8a84cff2469c6
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-08-09 22:59:05 +0300

    feat: method for initializing the database script

--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -9,6 +9,12 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class HibernateUtil {
 
     private static StandardServiceRegistry registry;
@@ -17,30 +23,50 @@ public class HibernateUtil {
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             try {
-
                 Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
-
 
                 registry = new StandardServiceRegistryBuilder()
                         .applySettings(configuration.getProperties())
                         .build();
 
-
                 MetadataSources sources = new MetadataSources(registry)
                         .addAnnotatedClass(Player.class)
                         .addAnnotatedClass(Match.class);
 
-
                 Metadata metadata = sources.getMetadataBuilder().build();
 
-
                 sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+                executeSqlScript("data.sql");
+
             } catch (Exception e) {
                 releaseResources();
                 throw new ExceptionInInitializerError(e);
             }
         }
         return sessionFactory;
+    }
+
+    private static void executeSqlScript(String scriptPath) {
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:h2:mem:test", "ohrim", "");
+             Statement statement = connection.createStatement();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(
+                     HibernateUtil.class.getClassLoader().getResourceAsStream(scriptPath)))) {
+
+            StringBuilder sql = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                sql.append(line).append("\n");
+            }
+
+            statement.execute(sql.toString());
+            System.out.println("SQL script executed successfully.");
+
+        } catch (Exception e) {
+            System.err.println("Failed to execute SQL script.");
+        }
     }
 
     public static void releaseResources() {
