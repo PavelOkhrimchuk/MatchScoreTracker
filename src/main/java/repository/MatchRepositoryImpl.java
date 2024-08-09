3d014da62fc77f36f23918cb0a45ce6ba735fdf1
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-08-09 17:57:27 +0300

    refactor: repository methods change

--- src/main/java/repository/MatchRepositoryImpl.java
+++ src/main/java/repository/MatchRepositoryImpl.java
@@ -1,59 +1,72 @@
 package repository;
 
+import lombok.Setter;
 import model.Match;
-import model.Player;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+import util.HibernateUtil;
 
 import java.util.List;
 import java.util.Optional;
-
+@Setter
 public class MatchRepositoryImpl implements MatchRepository {
-
-    private Session session;
-
-    public MatchRepositoryImpl(Session session) {
-        this.session = session;
-    }
-
 
     @Override
     public List<Match> findByPlayerName(String playerName) {
-        String hql = "SELECT m FROM Match m JOIN m.player1 p1 JOIN m.player2 p2 WHERE p1.name = :name OR p2.name = :name";
-        Query<Match> query = session.createQuery(hql, Match.class);
-        query.setParameter("name", playerName);
-        return query.list();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            String hql = "SELECT m FROM Match m JOIN m.player1 p1 JOIN m.player2 p2 WHERE p1.name = :name OR p2.name = :name";
+            Query<Match> query = session.createQuery(hql, Match.class);
+            query.setParameter("name", playerName);
+            return query.list();
+        }
     }
 
     @Override
     public void save(Match entity) {
-        Transaction transaction = session.beginTransaction();
-        session.persist(entity);
-        transaction.commit();
-
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.merge(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        }
     }
 
     @Override
     public Optional<Match> findById(Integer id) {
-        Match match = session.get(Match.class, id);
-        return Optional.ofNullable(match);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Match match = session.get(Match.class, id);
+            return Optional.ofNullable(match);
+        }
     }
 
     @Override
     public List<Match> findAll() {
-        return session.createQuery("FROM Match", Match.class).list();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("FROM Match", Match.class).list();
+        }
     }
 
     @Override
     public void delete(Integer id) {
-
-        Transaction transaction = session.beginTransaction();
-        Match match = session.get(Match.class, id);
-        if (match != null) {
-            session.remove(match);
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Match match = session.get(Match.class, id);
+            if (match != null) {
+                session.remove(match);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
         }
-        transaction.commit();
-
     }
 }
