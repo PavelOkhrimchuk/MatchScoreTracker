c999a74ac40c72f4b4b74e9baa49428dba124449
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-08-09 18:13:05 +0300

    feat: methods for pagination and filtering

--- src/main/java/repository/MatchRepository.java
+++ src/main/java/repository/MatchRepository.java
@@ -7,4 +7,8 @@ import java.util.List;
 public interface MatchRepository extends Repository<Match,Integer>{
 
     List<Match> findByPlayerName(String playerName);
+
+    List<Match> findMatchesWithPaginationAndFilter(int offset, int limit, String playerName);
+
+    Long countMatchesByPlayerName(String playerName);
 }
